3a413899f23c602e9eb3b119102bff7dc2d7082c
Mefjuszkov <matiso2311252@gmail.com>
2020-06-02 15:45:18 +0200

    Dodano funkcję dodawania zadań

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 require('./store').init()
-const parse = require('koa-bodyparser')
-const path = require('path')
+
 const Koa = require('koa')
 const serve = require('koa-static')
-app.use(parse())
+const parse = require('koa-bodyparser')
+
 const app = new Koa()
 const port = process.env.PORT || 3000
 
-app.use(serve(path.resolve(__dirname, '..', 'client')))
+app.use(parse())
+
+app.use(serve('client'))
 
 const userRoutes = require('./routes/users')
 app.use(userRoutes.routes())
@@ -18,3 +20,4 @@ app.use(taskRoutes.routes())
 app.listen(port)
 
 console.log('App is listening at http://127.0.0.1:3000')
+
